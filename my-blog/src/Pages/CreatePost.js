add02c449761329b5c79ab80943abd0e25635524
Mkisecok <mahmutkisecok@gmail.com>
2022-01-09 21:32:27 +0100

    Add TextField to CreatePost componenet

--- my-blog/src/Pages/CreatePost.js
+++ my-blog/src/Pages/CreatePost.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import { addDoc } from 'firebase/firestore';
+
 
 function CreatePost() {
+    const[ title, setTitle ] = useState('');
+    const[ postText, setPostText ] = useState('');
+
+    const createPost = async ()=>{
+        await addDoc()
+    }
+
     return (
-        <div>
-            CreatePost
+        <div className='createPostPage'>
+            <Box  component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' }, }}autoComplete="off">
+            <TextField 
+            id="title" 
+            label="Title" 
+            variant="outlined" 
+            onChange={(event)=>{setTitle(event.target.value)}}/>
+
+            <TextField 
+            id="post" 
+            label="Post..." 
+            multiline rows={6}  
+            onChange={(event)=>{setPostText(event.target.value)}}
+            />
+            <Button variant="contained">Submit Post</Button>
+
+            </Box>
         </div>
     )
 }
