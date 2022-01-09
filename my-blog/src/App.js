a4047d21aba0bbad68c856868a8c8201a8135752
Mkisecok <mahmutkisecok@gmail.com>
2022-01-09 13:36:54 +0100

    docs: Create Pages

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home';
 
 function App() {
   return (
     
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
+    </Router>
 
     
   );
