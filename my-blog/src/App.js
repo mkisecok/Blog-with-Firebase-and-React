b9b50e8239b223985f531cbeac1062563b4895f4
Mkisecok <mahmutkisecok@gmail.com>
2022-01-09 15:17:45 +0100

    styled: navbar

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,18 +1,37 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom'
 import Home from './Pages/Home';
+import CreatePost from './Pages/CreatePost';
+import Login from './Pages/Login';
 
-function App() {
+
+
+function App(props) {
+ 
+ 
   return (
     
+   
+
     <Router>
+      <nav>
+        <Link to='/'> Home</Link>
+        <Link to='/createpost'> Create Post</Link>
+        <Link to='/login'> Login</Link>
+      </nav>
+
+    
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/createpost' element={<CreatePost/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
       </Routes>
     </Router>
+  
+
+
+    
 
     
   );
