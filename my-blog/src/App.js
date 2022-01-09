4edb3ef0eda791253e4f0b89a3d4301b4c0fab51
Mkisecok <mahmutkisecok@gmail.com>
2022-01-09 19:31:15 +0100

    feat: Add auth from firebase and functionalty for auth

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,11 +4,25 @@ import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom'
 import Home from './Pages/Home';
 import CreatePost from './Pages/CreatePost';
 import Login from './Pages/Login';
-
+import { useState } from 'react';
+import  {signOut}  from 'firebase/auth'
+import { auth } from './firebase-config';
 
 
 function App(props) {
  
+  const[isAuth,setIsAuth]=useState(false);
+
+ 
+
+  const signUserOut = ()=>{
+
+    signOut(auth).then(()=>{
+      localStorage.clear()
+      setIsAuth(false)
+      window.location.pathname='/login'
+    })
+  }
  
   return (
     
@@ -18,14 +32,18 @@ function App(props) {
       <nav>
         <Link to='/'> Home</Link>
         <Link to='/createpost'> Create Post</Link>
-        <Link to='/login'> Login</Link>
+
+        {!isAuth 
+        ? <Link to='/login' className='login-link'> Login</Link>
+        : <Link to='#' className='login-link' onClick={signUserOut}> Log Out</Link>} 
+        
       </nav>
 
     
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/createpost' element={<CreatePost/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/login' element={<Login setIsAuth={setIsAuth}/>}></Route>
       </Routes>
     </Router>
   
--- my-blog/src/Pages/Login.js
+++ my-blog/src/Pages/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import { auth, provider } from '../firebase-config'
+import {signInWithPopup} from 'firebase/auth'
+import {useNavigate} from 'react-router-dom'
 
-function Login() {
-    return (
-        <div>
-            Login
+function Login({setIsAuth}) {
+    let navigate=useNavigate()
+
+    const signInWithGoogle = ()=>{
+        signInWithPopup(auth, provider).then((result)=>{
+            localStorage.setItem('isAuth',true)
+            setIsAuth(true)
+            navigate('/')
+        })
+    }
+   return (
+        <div className='loginPage'>
+           <p>Sign in With Google to Continue</p>
+           <Button className='login-with-goggle-btn' variant="contained" onClick={signInWithGoogle}>Sign in with Google</Button>
         </div>
     )
 }
